d0d43081e766009e04071a2ce5b4851a37aae2f1
PedroSpelta <p.h.s.8989@hotmail.com>
2021-11-29 22:27:57 -0300

    adicionando o heroicons

---
+++ components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/outline";
+
+function Sidebar() {
+  return (
+    <div>
+      <div>
+        <div>
+          <HomeIcon className="h-5 w-5" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
