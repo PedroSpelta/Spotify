34543f396aff4b1d3457c84a17c6274c9318e494
PedroSpelta <p.h.s.8989@hotmail.com>
2021-12-15 01:54:00 -0300

    mudando cor do texto

--- components/Song.jsx
+++ components/Song.jsx
@@ -20,7 +20,7 @@ function Song({ track, order }) {
   }
 
   return (
-    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer" onClick={playSong}>
+    <div className="grid grid-cols-2 text-gray-400 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer" onClick={playSong}>
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img
